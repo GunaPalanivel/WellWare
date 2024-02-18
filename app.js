c40f8a2c36f1738be579ffe0bd65b93241a2797c
GunaPalanivel <guna.gp59@gmail.com>
2024-02-18 21:34:05 +0530

    made changes in app.js

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db'); 
+const connectDB = require("./config/db");
 
 // Set up middleware
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 // Import routes
-const indexRoute = require('./routes/index');
-const doctorsRouter = require('./routes/doctors');
-const servicesRouter = require('./routes/services');
-const appointmentsRouter = require('./routes/appointments');
-const aboutRouter = require('./routes/about');
-const contactRouter = require('./routes/contact');
-const facilitiesRouter = require('./routes/facilities');
-const newsRouter = require('./routes/news');
-const patientsRouter = require('./routes/patients');
+const indexRoute = require("./routes/index");
+const doctorsRouter = require("./routes/doctors");
+const servicesRouter = require("./routes/services");
+const appointmentsRouter = require("./routes/appointments");
+const aboutRouter = require("./routes/about");
+const contactRouter = require("./routes/contact");
+const facilitiesRouter = require("./routes/facilities");
+const newsRouter = require("./routes/news");
+const patientsRouter = require("./routes/patients");
 // Import other route files as needed
 
 // Set up routes
-app.use('/', indexRoute);
-app.use('/doctors', doctorsRouter);
-app.use('/services', servicesRouter);
-app.use('/appointments', appointmentsRouter);
-app.use('/about', aboutRouter);
-app.use('/contact', contactRouter);
-app.use('/facilities', facilitiesRouter);
-app.use('/news', newsRouter);
-app.use('/patients', patientsRouter);
+app.use("/", indexRoute);
+app.use("/doctors", doctorsRouter);
+app.use("/services", servicesRouter);
+app.use("/appointments", appointmentsRouter);
+app.use("/about", aboutRouter);
+app.use("/contact", contactRouter);
+app.use("/facilities", facilitiesRouter);
+app.use("/news", newsRouter);
+app.use("/patients", patientsRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
 // Call connectDB function to establish the database connection
 connectDB();
 
-  const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
@@ -85,7 +85,6 @@ app.listen(port, () => {
 //   }
 // ]
 
-
 // [
 //   {
 //     name: "John Doe",
@@ -127,4 +126,3 @@ app.listen(port, () => {
 //     doctors: [ObjectId("6480301ab2f657830a106fa0")]
 //   }
 // ]
-
